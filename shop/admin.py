5d5069a29dae2d353e3c5730df6a64c07bbcedf7
arian radmehr <arian.radmehr.bw@gmail.com>
2021-07-20 21:25:35 +0430

    Registering catalog models on the administration site

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from shop.models import Product, Category
 
-# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'price', 'available', 'created', 'updated')
+    prepopulated_fields = {'slug': ('name',)}
+    list_filter = ('available', 'created', 'updated')
+    list_editable = ('price', 'available')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
