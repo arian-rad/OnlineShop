4bed6751dccba615bd7c0fa628b3c3e516d30f67
arian radmehr <arian.radmehr.bw@gmail.com>
2021-07-24 14:11:29 +0430

    Including order models in the administration site and Creating customer orders

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.views.generic import View
+from orders.models import OrderItem
+from orders.forms import OrderCreateForm
+from cart.cart import Cart
 
-# Create your views here.
+
+class OrderCreateView(View):  # Didn't use CreateView because I was facing multiple models: OrderItem, Order
+    def get(self, request):
+        cart = Cart(request)
+        form = OrderCreateForm()
+        return render(request, 'orders/order_create_view.html', {'cart': cart, 'form': form})
+
+    def post(self, request):
+        cart = Cart(request)
+        form = OrderCreateForm(request.POST)
+        if form.is_valid():
+            order = form.save()
+            for item in cart:
+                OrderItem.objects.create(order=order, product=item['product'], price=item['price'],
+                                         quantity=item['quantity'])
+            cart.clear()
+            return render(request, 'orders/success.html', {'order': order})
