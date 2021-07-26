3b034867e15c9405fe441aa8b1749c06fa7426fa
arian radmehr <arian.radmehr.bw@gmail.com>
2021-07-26 10:36:06 +0430

    Adding asynchronous tasks to application

--- core/settings.py
+++ core/settings.py
@@ -126,4 +126,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
--- orders/views.py
+++ orders/views.py
@@ -3,6 +3,7 @@ from django.views.generic import View
 from orders.models import OrderItem
 from orders.forms import OrderCreateForm
 from cart.cart import Cart
+from orders.tasks import order_created
 
 
 class OrderCreateView(View):  # Didn't use CreateView because I was facing multiple models: OrderItem, Order
@@ -20,4 +21,6 @@ class OrderCreateView(View):  # Didn't use CreateView because I was facing multi
                 OrderItem.objects.create(order=order, product=item['product'], price=item['price'],
                                          quantity=item['quantity'])
             cart.clear()
+            print("order:", order)
+            # order_created.delay(order.id)  # launching asynchronous task
             return render(request, 'orders/success.html', {'order': order})
