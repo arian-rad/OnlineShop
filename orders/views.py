20314b237236db5c7c868de28f1c07991a4562c9
arian radmehr <arian.radmehr.bw@gmail.com>
2021-07-27 13:10:05 +0430

    Rendering PDF files with error:OSError: no library called cairo was found

--- orders/views.py
+++ orders/views.py
@@ -6,6 +6,10 @@ from cart.cart import Cart
 import zarinpal
 from django.contrib.admin.views.decorators import staff_member_required
 from django.utils.decorators import method_decorator
+from django.conf import settings
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+import weasyprint
 
 
 class OrderCreateView(View):  # Didn't use CreateView because I was facing multiple models: OrderItem, Order
@@ -38,3 +42,22 @@ class AdminOrderDetailView(TemplateView):
         context = super(AdminOrderDetailView, self).get_context_data(**kwargs)
         context['order'] = get_object_or_404(Order, id=self.kwargs['order_id'])
         return context
+
+
+@method_decorator(staff_member_required, name='dispatch')
+class AdminOrderPDFView(TemplateView):
+    """
+    This is the view to generate a PDF invoice for an order.
+    """
+    template_name = ''
+
+    def get_context_data(self, **kwargs):
+        context = super(AdminOrderPDFView, self).get_context_data(**kwargs)
+        order = get_object_or_404(Order, id=self.kwargs['order_id'])
+        html = render_to_string('orders/pdf.html', {'order': order})
+        response = HttpResponse(content_type='application/pdf')
+        response['Content-Disposition'] = f'filename=order_{order.id}.pdf'
+        weasyprint.HTML(string=html).write_pdf(response, stylesheets=[weasyprint.CSS(settings.STATIC_ROOT + 'css/pdf'
+                                                                                                            '.css')])
+        return response
+
