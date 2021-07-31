9314f2fa2eb9a3e0f8162ae48b5df29cde26fdb3
arian radmehr <arian.radmehr.bw@gmail.com>
2021-07-31 16:33:41 +0430

    Adding a language prefix to URL patterns

--- core/urls.py
+++ core/urls.py
@@ -2,15 +2,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
 
-urlpatterns = [
+urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
     path('cart/', include('cart.urls', namespace='cart')),
     path('orders/', include('orders.urls', namespace='orders')),
     path('payment/', include('zarinpal.urls', namespace='zarinpal')),
     path('coupons/', include('coupons.urls', namespace='coupons')),
     path('shop/', include('shop.urls', namespace='shop')),  # URLs for the app "shop"
-]
+)
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
