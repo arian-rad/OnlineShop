03464ccf0b1418f3b18f56f8b6c7a85c91fd54e2
arian radmehr <arian.radmehr.bw@gmail.com>
2021-07-31 17:45:44 +0430

    Allowing users to switch language

--- shop/models.py
+++ shop/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.utils.text import slugify  # Not mentioned in Creating product catalog models task
 from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=200, db_index=True)
+    name = models.CharField(_('name'), max_length=200, db_index=True)
     slug = models.SlugField(max_length=200, unique=True)
 
     class Meta:
@@ -24,12 +25,12 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=200, db_index=True)
+    name = models.CharField(_('name'), max_length=200, db_index=True)
     slug = models.SlugField(max_length=200, db_index=True)
-    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE, verbose_name=_('category'))
     image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
-    description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField(_('description'), blank=True)
+    price = models.DecimalField(_('price'), max_digits=10, decimal_places=2)
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
